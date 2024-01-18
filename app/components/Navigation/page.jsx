6bff60e8f307fef4c9426a31a6e75baf5ac84c1d
mouhamedhanne <mouhamedhanne72@gmail.com>
2024-01-18 18:02:20 +0000

    init

--- app/components/Navigation/page.jsx
+++ app/components/Navigation/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import Buttons from "@/app/ui/Buttons/page";
+import Buttons from "@/app/ui/buttons/page";
 import Link from "next/link";
 import Logo from "@/app/Assets/Logo";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
